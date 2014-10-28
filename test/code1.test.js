1e93745f724c147a7ae6d0b936931a6da0c97c8a
Koustuv Sinha <koustuvsinha@hotmail.com>
2014-10-28 17:27:18 +0530

    bug fix

--- test/code1.test.js
+++ test/code1.test.js
@@ -1,15 +1,18 @@
 describe("Test suite for 1.1",function() {
     it("Testing for patternToNumber using hard coded values",function() {
-        expect(patternToNumber("AA").toBe(0));
+        var hmp = require('./js/code1');
+        expect(hmp.patternToNumber("AA").toBe(0));
     });
     it("Testing for NumberToPattern using hard coded values",function() {
-        expect(NumberToPattern(21,5).toBe("AACCC"));
+        var hmp = require('./js/code1');    
+        expect(hmp.NumberToPattern(21,5).toBe("AACCC"));
     });
     it("Testing patternToNumber and NumberToPattern by randomized inputs",function() {
+        var hmp = require('./js/code1');
         var randomLength = Math.floor(Math.random()*10 + 1);
         var randomNumber = Math.floor(Math.random()*(Math.pow(4,randomLength)) + 1);
-        var pattern = NumberToPattern(randomNumber,randomLength);
-        var numberFromPattern = patternToNumber(pattern);
+        var pattern = hmp.NumberToPattern(randomNumber,randomLength);
+        var numberFromPattern = hmp.patternToNumber(pattern);
         
         expect(numberFromPattern).toBe(randomNumber));
     });

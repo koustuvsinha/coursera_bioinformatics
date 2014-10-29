33217216d821ae4a49d93920c770fa91ebd8cd56
Koustuv Sinha <koustuvsinha@hotmail.com>
2014-10-29 10:37:39 +0530

    Added jasmine-node test cases

--- spec/code1.spec.js
+++ spec/code1.spec.js
@@ -1,11 +1,11 @@
 describe("Test suite for 1.1",function() {
     it("Testing for patternToNumber using hard coded values",function() {
         var hmp = require('../js/code1');
-        expect(hmp.patternToNumber("AA").toBe(0));
+        expect(hmp.patternToNumber("AA")).toBe(0);
     });
     it("Testing for NumberToPattern using hard coded values",function() {
         var hmp = require('../js/code1');    
-        expect(hmp.NumberToPattern(21,5).toBe("AACCC"));
+        expect(hmp.NumberToPattern(21,5)).toBe("AACCC");
     });
     it("Testing patternToNumber and NumberToPattern by randomized inputs",function() {
         var hmp = require('../js/code1');
@@ -14,7 +14,7 @@ describe("Test suite for 1.1",function() {
         var pattern = hmp.NumberToPattern(randomNumber,randomLength);
         var numberFromPattern = hmp.patternToNumber(pattern);
         
-        expect(numberFromPattern).toBe(randomNumber));
+        expect(numberFromPattern).toBe(randomNumber);
     });
                
 });
